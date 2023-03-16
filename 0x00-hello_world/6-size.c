c3eeaaa28e97f29300211521e3d134de1fb046c4
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-03-16 15:24:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,3 +11,4 @@ printf("Size of a long int: %ln byte(s)", sizeof(long int));
 printf("Size of a long long int: %ln byte(s)", sizeof(long long int));
 printf("Size of a float: %ln byte(s)", sizeof(float));
 return (0);
+}
