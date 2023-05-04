521656f5ba34493690c7616b46f312fb1ee5bcc2
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-04 00:30:57 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcpy -  copies the string pointed to by src, including the terminating
+*null byte to the buffer pointed to by dest
+*@dest: is apointer indicates to the buffer wherethe string is coppied
+*@src: is apointer indicates to the buffer containes the string to be coppied
+*Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, crs));
+}
