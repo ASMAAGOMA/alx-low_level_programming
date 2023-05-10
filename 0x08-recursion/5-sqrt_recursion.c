3f604395446274bc0278bd616d1020969b6b19be
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-10 13:36:14 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include "main.h"
+
+int helper(int n, int i);
+
+
+/**
+ * _sqrt_recursion - a function that returns
+ * the natural square root of a number
+ * @n: the numbe to get the square root to
+ * Return: the value of sqr root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (helper(n, 0));
+	}
+}
+
+/**
+ * helper - a function that returns
+ * the natural square root of a number
+ * @n: the numbe to get the square root to
+ * @i: the square root to n
+ * Return: the value of sqr root
+ */
+int helper(int n, int i)
+{
+		if (i * i > n)
+		{
+			return (-1);
+		}
+		else if (i * i > n)
+		{
+			return (i);
+		}
+		else
+		{
+			return (helper(n, i + 1));
+		}
+}
