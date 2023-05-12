9f0ae06582ad1f021d286fce5aa271702cff5fff
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-12 16:10:30 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main - the main function
+ *@argc: number of argument
+ *@argv: array of arguments
+ *Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int resuit = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
