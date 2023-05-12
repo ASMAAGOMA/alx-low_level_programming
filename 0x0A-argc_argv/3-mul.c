fade2ae0c9ee1ec17d85b9044423571fb2577059
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-12 16:12:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
-	int resuit = num1 * num2;
+	int result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
