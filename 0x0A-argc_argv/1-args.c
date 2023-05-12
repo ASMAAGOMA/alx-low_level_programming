6080c8fed37f011847612ebf603ae9e771b5c9f4
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-12 14:27:23 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function return an integer and take
+ * the value of argv and argc
+ * Return: always 0
+ * @argv: number of arguments
+ * @argc: the strings og arguments
+ */
+
+int main(int argv, char *argc[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
