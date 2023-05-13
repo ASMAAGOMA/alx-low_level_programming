e3f250ab87d1990bd52a7bea7122eed7369b60ac
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-12 18:54:16 -0700

    _abs.c

---
+++ 0x09-static_libraries/_abs.c
@@ -0,0 +1,11 @@
+/**
+ *_abs - computes the absolute value of an integer
+ *
+ *@n: integer to compute absolute value of
+ *
+ *Return: absolute value of n
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
