be102434424aa9ece841e6c86af26d6f5bb07884
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-14 15:21:17 -0700

    2-strlen.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_puts - prints a string to stdout
+ *
+ *@str: the string to be printed
+ *
+ *Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
