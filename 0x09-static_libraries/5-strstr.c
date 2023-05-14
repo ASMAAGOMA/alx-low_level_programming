246b31bb70063592f29aa15efb13bd2251ae813a
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-14 15:51:59 -0700

    2-strlen.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strstr - Locate a substring
+ *@haystack: The string to search
+ *@needle: The substring to search for
+ *
+ *Return: Pointer to the beginning of the located substring,
+ *or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+i{
+	char *start_haystack, *start_needle;
+
+	while (*haystack)
+	{
+		start_haystack = haystack;
+		start_needle = needle;
+
+		while (*start_haystack && *start_needle && *start_haystack == *start_needle)
+		{
+			start_haystack++;
+			start_needle++;
+		}
+
+		if (!*start_needle)
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
