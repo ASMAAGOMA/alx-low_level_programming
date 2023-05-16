7b57be3617ef52b1fee8d720c1e9cc2b792f0ae0
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-16 13:04:25 -0700

    finaloutput

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "main.h"
-
-/**
- * main - a function that concatenates two strings
- * Return: the value of conatenated string
- *@s1: string to be conatenated
- *@s2: string to be conatenated
- *
- */
-
-char *str_concat(char *s1, char *s2);
-
-int main(void)
-{
-	char *s1 = "Hello, ";
-	char *s2 = "world!";
-
-	char *concatenated = str_concat(s1, s2);
-
-	if (concatenated == NULL)
-	{
-		printf("Failed to allocate memory\n");
-		return (1);
-	}
-
-	printf("Concatenated string: %s\n", concatenated);
-
-	free(concatenated);
-	return (0);
-}
-
+#include <stdlib.h>
 /**
  *str_concat - a function that concatenates two strings
  *@s1: string to be conatenated
