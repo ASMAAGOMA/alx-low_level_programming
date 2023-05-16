153dd8fd0cac02f125b68b1e51dd9ca881b2c06b
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-16 11:35:58 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - a function that concatenates two strings
+ * @s1: string to be conatenated
+ * @s2: string to be conatenated
+ * Return: the value of conatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t s1len;
+	size_t s2len;
+
+	s1len = strlen(s1);
+
+	s2len = strlen(s2);
+	char *concatenated;
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	concatenated = malloc((strlen(s1)) + (strlen(s2)) + 1 * sizeof(char));
+
+	if (concatenated == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(concatenated, s1);
+	strcat(concatenated, s2);
+}
+
+
