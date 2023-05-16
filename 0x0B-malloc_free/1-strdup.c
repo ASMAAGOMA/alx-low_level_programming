21affd5418b0047ecce53a6aeba566cf25afb88d
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-16 14:02:06 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,33 +1,29 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * _strdup - a function that returns a pointer
- * to a newly allocated space in memory
- * which contains a copy of the string given as a parameter
- * @str: a copy of the string
- * Return: the value of a copy of the string
+ *_strdup - Duplicates a string.
+ *@str: The string to be duplicated.
+ *
+ *Return: Pointer to the newly allocated duplicated string,
+ *or NULL if str is NULL or if memory allocation fails.
  */
 
 char *_strdup(char *str)
 {
 	size_t stringlen = strlen(str);
-	char *duplication;
+
+	char *duplication = malloc((stringlen + 1) * sizeof(char));
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	duplication = malloc((stringlen + 1) * sizeof(char));
 	if (duplication == NULL)
 	{
-		return (NULL);
+		return (NULL); 
 	}
 	strcpy(duplication, str);
+
 	return (duplication);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that returns a pointer to a
+ * 2 dimensional array of integers
+ * @width: 
