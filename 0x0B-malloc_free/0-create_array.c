7563eb6b4e59c88f78013b1b233c076f2a840440
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-16 09:57:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 	{
