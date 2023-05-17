be0e8fb595a3b57b16bb11a5e5b82301d0d65e80
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-16 17:19:42 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+
+/**
+ * free_grid - that frees a 2 dimensional grid
+ * @grid: the 2 dimensional grid to be freed
+ * @height: the rows number
+ * Return: return
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
