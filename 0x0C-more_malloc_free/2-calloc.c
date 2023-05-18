012cbdf24c3232b77588957d8927a9da724e1698
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-05-18 15:38:29 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc -  a function that allocates memory for an array
+ * using malloc
+ * @nmemb: an array
+ *@size: size of an array
+ *Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, size * nmemb);
+
+	return (ptr);
+}
+
