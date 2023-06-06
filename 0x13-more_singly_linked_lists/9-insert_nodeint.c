e7ee9214b8327066fd6ac4552f34a4c4b97d2eb6
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-06-06 15:00:00 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 
 /**
  * insert_nodeint_at_index - function that inserts a
- * new node at a given position
+ * new node at a given p`osition
  * @head: first node of the list
  * @idx: the index of the list where the new node
  * should be added. Index starts at 0
@@ -13,33 +13,37 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	unsigned int count;
+	unsigned int position;
 	listint_t *new_node;
+	listint_t *current_node = *head;
 
-	count = 0;
-	if (head == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
 		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (idx == 0)
 	{
-		if (*head == NULL)
-			return (add_nodeint_end(head, n));
-		else
-			return (add_nodeint_end(head, n));
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	current = *head;
-	while (current != NULL && count < idx - 1)
+
+	for (position = 0; current_node && position < idx; position++)
 	{
-		current = current->next;
-		count++;
+		if (position == idx - 1)
+		{
+			new_node->next = current_node->next;
+			current_node->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			current_node = current_node->next;
+		}
 	}
-	if (current == NULL || count == 0)
-		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
-	return (new_node);
+
+	return (NULL);
 }
