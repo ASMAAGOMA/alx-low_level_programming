e15af6acb5c006468673f579202753ca6acbbe00
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-06-08 16:54:15 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function that checks the endianness
+ * Return: 1(little), 0(big)
+ */
+
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	unsigned char *ptr = (unsigned char*)&number;
+
+	if (*ptr != 0)
+		return (1);
+	else
+		return (0);
+}
