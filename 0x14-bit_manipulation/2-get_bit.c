48efcaf3bd7aa72fd9d7322fb2cb14e418e36362
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-06-08 15:22:16 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,6 +15,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask = 1UL << index;
 
 	if (index >= sizeof(unsigned long int) * 8)
-		return (0);
+		return (-1);
 	return ((n & mask) != 0);
 }
