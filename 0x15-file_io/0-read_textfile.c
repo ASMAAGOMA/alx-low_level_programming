bc50a5bca6af0f1805e249668ad8bc34db314af1
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-06-12 16:50:36 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: file name
+ * @letters: letters
+ * Return: if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+	bytes_read = fread(buffer, sizeof(char), letters, file);
+	if (bytes_read < 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written < 0 || (size_t)bytes_written != (size_t)bytes_read)
+	{
+		fclose(file);
+		return (0);
+		free(buffer);
+	}
+	free(buffer);
+	return (0);
+	return (bytes_read);
+}
+
