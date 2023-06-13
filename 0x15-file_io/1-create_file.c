f8b4754f88f0b5d4aad3d3f712f91a4f5c93022b
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-06-12 17:53:31 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
- /**
-  *create_file - function that creates a file with the given context
-  * @filename: is the name of the file to create
-  * @ is a NULL terminated string to write to the file
-  * Returns: 1 on success, -1 on failure (file can not be created,
-  * file can not be written, write “fails”, etc…)
-  * The created file must have those permissions: rw-------.
-  * If the file already exists, do not change the permissions.
-  * if the file already exists, truncate it
-  * if filename is NULL return -1
-  */
+/**
+ * create_file - a function that creates a file
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 or -1
+ */
 
 int create_file(const char *filename, char *text_content)
 {
