bf4b8cee267d32474fec76bb27faada0bfad1f22
ASMAAGOMA <125485265+ASMAAGOMA@users.noreply.github.com>
2023-07-20 23:41:54 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,21 +9,21 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *new_node = malloc(sizeof(dlistint_t));
-        if (new_node == NULL)
-                return (NULL);
-        new_node->n = n;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 
-        if (*head == NULL)
-        {
-                new_node->next = new_node->prev = NULL;
-        }
-        else
-        {
-                new_node->next = *head;
-                new_node->prev = NULL;
-                (*head)->prev = new_node;
-        }
-        *head = new_node;
-        return (new_node);
+	if (*head == NULL)
+	{
+		new_node->next = new_node->prev = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+		new_node->prev = NULL;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
 }
