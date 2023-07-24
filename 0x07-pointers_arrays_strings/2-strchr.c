88b5ce0e0ae2a15a8ca0d2a8b55aef45ba8610bf
ASMAAGOMA <125485265+ASMAAGOMA@users.noreply.github.com>
2023-07-24 20:17:10 +0000

    aa

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,6 @@
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string to search in.
  * @c: Character to be located.
- *
  * Return: Pointer to the first occurrence of the character c in the string s,
  * or NULL if the character is not found.
  */
