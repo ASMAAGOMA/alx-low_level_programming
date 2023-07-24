173536a65706a7d8555256b02f7700f04932fbeb
ASMAAGOMA <125485265+ASMAAGOMA@users.noreply.github.com>
2023-07-24 19:23:55 +0000

    aa

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi -  convert a string to an integer
+ * @s: string
+ * Return: result * sign
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+
+	while (*s && (*s >= '0' && *s <= '9'))
+	{
+
+		int digit = *s - '0';
+		if (result > (INT_MAX - digit) / 10)
+		{
+
+			return ((sign == 1) ? INT_MAX : INT_MIN);
+		}
+		result = result * 10 + digit;
+		s++;
+	}
+
+	return (result * sign);
+}
