666d9e091dffa198a94565074505af9c83ec7a20
ASMAAGOMA <asmaagadallaah@gmail.com>
2023-08-11 16:31:22 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
-
 /**
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table to add the value to.
@@ -16,10 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || !*key || !value)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
@@ -32,18 +29,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (!new_node->key)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (!new_node->value)
 	{
@@ -51,9 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
