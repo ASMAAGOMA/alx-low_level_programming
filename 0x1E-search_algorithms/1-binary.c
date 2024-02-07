35f7456452f78d8244a105e6d0c81d343dcd33d2
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-07 08:59:09 +0200

    :"(

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,33 +12,33 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t mid;
-    size_t left, right;
+	size_t mid;
+	size_t left, right;
 
-    if (array == NULL)
-        return (-1);
-    right = size - 1;
-    left = 0;
-    while (left <= right)
-    {
-        size_t i;
+	if (array == NULL)
+		return (-1);
+	right = size - 1;
+	left = 0;
+	while (left <= right)
+	{
+		size_t i;
 
-        printf("Searching in array:");
-        for (i = left; i <= right; i++)
-        {
-            if (i != left)
-                printf(",");
-            printf(" %d", array[i]);
-        }
-        printf("\n");
-        mid = (left + right) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		printf("Searching in array:");
+		for (i = left; i <= right; i++)
+		{
+			if (i != left)
+				printf(",");
+			printf(" %d", array[i]);
+		}
+		printf("\n");
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
