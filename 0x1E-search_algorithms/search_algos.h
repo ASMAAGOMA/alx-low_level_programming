c6d29a017e46cf881a11a05a90a67458d7f01c96
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-09 01:27:18 +0200

    :"(

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,13 +5,7 @@
 #include <stdlib.h>
 #include <stdlib.h>
 
-/**
- * search algorithms - search algorithms
- * 
- * @array: array
- * @size: size
- * @value: value
-*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
 #endif
