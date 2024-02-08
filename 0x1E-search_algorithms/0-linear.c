26382a84ff860009f3728cbf0b6b55db633a46e1
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-09 01:20:30 +0200

    :"(

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * linear_search - searches for a value in an array of integers
- * using linear search.
+ * linear_search - searches for a value in a sorted array of integers
+ * using the linear_search algorithm
+ * @array: pointer to the first element of the array to search into
+ * @size: number of elements in the array
+ * @value: value to search for
  *
- * @array: array of integers
- * @size: size of the array
- * @value: the value to search for
- *
- * Return: the index where @value is in the array or -1
+ * Return: index where value is located, or -1 if not found
  */
 int linear_search(int *array, size_t size, int value)
 {
